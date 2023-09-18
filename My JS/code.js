0450f26fe41a334c16382944b479cf12502313dd
MrStar <mr.sadstar@gmail.com>
2023-09-18 14:51:23 -0300

    Add files via upload

---
+++ My JS/code.js
@@ -0,0 +1,18 @@
+
+function imprime(){
+    let lg = document.getElementById("login").value
+    let ps = document.getElementById("pass").value
+
+    let senha = "snoopy"
+
+    if(ps == senha){
+        alert("Voê está logado!")
+    }
+    else{
+        alert("Seu login esta errado")
+    }
+}
+function criar(){
+    let senha = document.getElementById("loginsenha").value
+    let login = document.getElementById("loginsenha").value
+}
